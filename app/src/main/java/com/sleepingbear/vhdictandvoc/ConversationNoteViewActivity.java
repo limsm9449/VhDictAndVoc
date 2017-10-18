284ac0be433e6c1b81424051708d0dae357be8ad
limsm9449 <limsm9449@naver.com>
2017-10-18 22:14:54 +0900

    2017.10.18

--- app/src/main/java/com/sleepingbear/vhdictandvoc/ConversationNoteViewActivity.java
+++ app/src/main/java/com/sleepingbear/vhdictandvoc/ConversationNoteViewActivity.java
@@ -310,7 +310,7 @@ public class ConversationNoteViewActivity extends AppCompatActivity implements V
                 if ( !adapter.isCheck() ) {
                     Toast.makeText(this, "선택된 데이타가 없습니다.", Toast.LENGTH_SHORT).show();
                 } else {
-                    new android.app.AlertDialog.Builder(this)
+                    new android.support.v7.app.AlertDialog.Builder(this)
                             .setTitle("알림")
                             .setMessage("삭제하시겠습니까?")
                             .setPositiveButton("확인", new DialogInterface.OnClickListener() {
